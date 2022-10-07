dafd6b6e3ca8784fdf0ee3891385f04e1479654b
Rahaf Nasrallah <98306265+rahafnasralla@users.noreply.github.com>
2022-10-08 01:14:03 +0300

    src

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Names from './Names';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Names array = {[{name: 'Ahmad', age: 30},
+                     {name: 'Yousef', age: 12},
+                     {name: 'Shatha', age: 14},
+                     {name: 'Rana', age: 22},
+                     {name: 'Osama', age: 22},
+                     {name: 'Ahmad', age: 38}]}/>
+    </div>
+  );
+}
+
+export default App;
