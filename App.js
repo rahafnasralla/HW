2d9965f7c2673240fae7821d06005686b40e8df1
Rahaf Nasrallah <98306265+rahafnasralla@users.noreply.github.com>
2022-10-10 16:24:34 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+
 import './App.css';
-import Names from './Names';
+import Hi from './Hi';
+
 
 
 function App() {
   return (
     <div className="App">
-     <Names array={[{name: 'Ahmad', age: 30},{name: 'Yousef', age: 12},{name: 'Shatha', age: 14},{name: 'Rana', age: 22},{name: 'Osama', age: 22},{name: 'Ahmad', age: 38}]}/>
+    <Hi/>
     </div>
   );
 }
-export default App;
 
+export default App;
