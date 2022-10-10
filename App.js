392b670bf43bd1222db92a46f5cb2351e5a68e8e
Rahaf Nasrallah <98306265+rahafnasralla@users.noreply.github.com>
2022-10-10 17:57:03 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import HW from './HW';
 function App() {
   return (
     <div className="App">
-     {/* <Names array={[{name: 'Ahmad', age: 30},{name: 'Yousef', age: 12},{name: 'Shatha', age: 14},{name: 'Rana', age: 22},{name: 'Osama', age: 22},{name: 'Ahmad', age: 38}]}/> */}
     <HW/>
     </div>
   );
